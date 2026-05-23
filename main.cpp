fec62eb3facf37a0bd321835dc9c9347550668dd
Zihanyang147 <Zihanyang147@users.noreply.github.com>
2026-05-22 21:40:29 -0700

    finish pa02

--- main.cpp
+++ main.cpp
@@ -1,24 +1,16 @@
-// Winter'24
-// Instructor: Diba Mirza
-// Student name: 
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <ctime>
 #include <vector>
-#include <cstring>
 #include <algorithm>
-#include <limits.h>
 #include <iomanip>
-#include <set>
-#include <queue>
-#include <sstream>
 using namespace std;
 
 #include "utilities.h"
 #include "movies.h"
 
 bool parseLine(string &line, string &movieName, double &movieRating);
+bool samePrefix(const string &name, const string &prefix);
 
 int main(int argc, char** argv){
     if (argc < 2){
@@ -34,23 +26,29 @@ int main(int argc, char** argv){
         exit(1);
     }
   
-    // Create an object of a STL data-structure to store all the movies
+    vector<Movie> movies;
 
     string line, movieName;
     double movieRating;
-    // Read each file and store the name and rating
+
     while (getline (movieFile, line) && parseLine(line, movieName, movieRating)){
-            // Use std::string movieName and double movieRating
-            // to construct your Movie objects
-            // cout << movieName << " has rating " << movieRating << endl;
-            // insert elements into your data structure
+        Movie m;
+        m.name = movieName;
+        m.rating = movieRating;
+        movies.push_back(m);
     }
 
     movieFile.close();
 
+    sort(movies.begin(), movies.end(), [](const Movie &a, const Movie &b) {
+        return a.name < b.name;
+    });
+
     if (argc == 2){
-            //print all the movies in ascending alphabetical order of movie names
-            return 0;
+        for (int i = 0; i < (int)movies.size(); i++) {
+            cout << movies[i].name << ", " << fixed << setprecision(1) << movies[i].rating << endl;
+        }
+        return 0;
     }
 
     ifstream prefixFile (argv[2]);
@@ -67,19 +65,65 @@ int main(int argc, char** argv){
         }
     }
 
-    //  For each prefix,
-    //  Find all movies that have that prefix and store them in an appropriate data structure
-    //  If no movie with that prefix exists print the following message
-    cout << "No movies found with prefix "<<"<replace with prefix>" << endl;
+    vector<Movie> bestMovies;
+    vector<string> bestPrefixes;
+    bool lastHadMovies = false;
 
-    //  For each prefix,
-    //  Print the highest rated movie with that prefix if it exists.
-    cout << "Best movie with prefix " << "<replace with prefix>" << " is: " << "replace with movie name" << " with rating " << std::fixed << std::setprecision(1) << "replace with movie rating" << endl;
+    for (int i = 0; i < (int)prefixes.size(); i++) {
+        string prefix = prefixes[i];
+        vector<Movie> matches;
+
+        Movie key;
+        key.name = prefix;
+        key.rating = 0;
+
+        vector<Movie>::iterator it = lower_bound(movies.begin(), movies.end(), key,
+            [](const Movie &a, const Movie &b) {
+                return a.name < b.name;
+            });
+
+        while (it != movies.end() && samePrefix(it->name, prefix)) {
+            matches.push_back(*it);
+            it++;
+        }
+
+        if (matches.empty()) {
+            cout << "No movies found with prefix " << prefix << endl;
+            lastHadMovies = false;
+        } else {
+            sort(matches.begin(), matches.end(), [](const Movie &a, const Movie &b) {
+                if (a.rating == b.rating) {
+                    return a.name < b.name;
+                }
+                return a.rating > b.rating;
+            });
+
+            for (int j = 0; j < (int)matches.size(); j++) {
+                cout << matches[j].name << ", " << fixed << setprecision(1) << matches[j].rating << endl;
+            }
+
+            bestPrefixes.push_back(prefix);
+            bestMovies.push_back(matches[0]);
+            lastHadMovies = true;
+        }
+
+        if (i != (int)prefixes.size() - 1) {
+            cout << endl;
+        }
+    }
+
+    if (lastHadMovies) {
+        cout << endl;
+    }
+
+    for (int i = 0; i < (int)bestMovies.size(); i++) {
+        cout << "Best movie with prefix " << bestPrefixes[i] << " is: "
+             << bestMovies[i].name << " with rating "
+             << fixed << setprecision(1) << bestMovies[i].rating << endl;
+    }
 
     return 0;
 }
-
-/* Add your run time analysis for part 3 of the assignment here as commented block*/
 
 bool parseLine(string &line, string &movieName, double &movieRating) {
     int commaIndex = line.find_last_of(",");
@@ -88,5 +132,19 @@ bool parseLine(string &line, string &movieName, double &movieRating) {
     if (movieName[0] == '\"') {
         movieName = movieName.substr(1, movieName.length() - 2);
     }
+    return true;
+}
+
+bool samePrefix(const string &name, const string &prefix) {
+    if (prefix.length() > name.length()) {
+        return false;
+    }
+
+    for (int i = 0; i < (int)prefix.length(); i++) {
+        if (name[i] != prefix[i]) {
+            return false;
+        }
+    }
+
     return true;
 }
